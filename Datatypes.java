b12995e3e59e97a702f44b74d39e1b817bb00071
codetestscsworld <ranabothuammu@gmail.com>
2026-02-10 13:34:15 +0530

    project

---
+++ Datatypes.java
@@ -0,0 +1,21 @@
+Class Datatypes
+{
+public static void main(String[] args)
+{
+byte b=123;//1 byte=8bits
+short s=32767;//2 bytes=16bits
+int i=2354368;//4 bytes=32bits
+long l=878238483;//8 bytes=64 bits
+float f=12.2324343f;//4 bytes=32 bits
+double d=132.2343433;//8 bytes=64 bits
+char ch='s';
+Boolean b1=true;
+System.out.println("the value of byte is"+b);
+System.out.println("the value of short is"+s);
+System.out.println("the value of int is"+i);
+System.out.println("the value of long is"+l);
+System.out.println("the value of float is"+f);
+System.out.println("the value of double is"+d);
+System.out.println("the value of char is"+ch);
+System.out.println("the value of Boolean is"+b1);
+}
